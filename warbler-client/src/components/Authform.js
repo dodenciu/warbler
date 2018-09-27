8fdf260cb78fb6529fdb00b1f8974054834d1349
Vlad Daniel Dodenciu <vlad.daniel.dodenciu@gmail.com>
2018-09-27 11:54:06 +0000

    Messages functionality

--- warbler-client/src/components/Authform.js
+++ warbler-client/src/components/Authform.js
@@ -1,101 +1,123 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Authform extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: "",
-            username: "",
-            password: "",
-            profileImageUrl: ""
-        };
-        this.handleChange = this.handleChange.bind(this);
-    }
-    
-    handleChange = e => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
-    
-    handleSubmit = e => {
-        e.preventDefault();
-        const authType = this.props.signup ? "signup" : "signin";
-        this.props.onAuth(authType, this.state)
-        .then(() => {
-           this.props.history.push("/");
-        }).catch(() => {
-            return;
-        })
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      username: "",
+      password: "",
+      profileImageUrl: ""
     };
-    
-    render() {
-        // eslint-disable-next-line
-        const { email, username, password, profileImageUrl } = this.state;
-        const { heading, buttonText, signup, errors, removeError, history } = this.props;
-        
-        history.listen(() => {
-            removeError();
-        });
-        
-        return (
-            <div>    
-                <div className="row justify-content-md-center text-center">
-                    <div className="col-md-6">
-                        <form onSubmit={this.handleSubmit}>
-                            <h2>{heading}</h2>
-                            {errors.message && 
-                                <div className="alert alert-danger">
-                                    {errors.message}
-                                </div>
-                            }
-                            <label htmlFor="email">Email:</label>
-                            <input 
-                                id="email" 
-                                name="email"
-                                value={email}
-                                type="text"
-                                className="form-control" 
-                                onChange={this.handleChange} 
-                            />
-                            <label htmlFor="password">Password:</label>
-                            <input 
-                                id="password" 
-                                name="password"
-                                type="password"
-                                className="form-control" 
-                                onChange={this.handleChange} 
-                            />
-                            {signup && (
-                                <div>
-                                    <label htmlFor="username">Username:</label>
-                                    <input 
-                                        id="username" 
-                                        name="username"
-                                        value={username}
-                                        type="text"
-                                        className="form-control" 
-                                        onChange={this.handleChange} 
-                                    />
-                                    <label htmlFor="image-url">Image:</label>
-                                    <input 
-                                        id="image-url" 
-                                        name="profileImageUrl"
-                                        value={profileImageUrl}
-                                        className="form-control" 
-                                        onChange={this.handleChange} 
-                                    />
-                                </div>
-                            )}
-                            <button type="submit" className="btn btn-primary btn-block btn-lg">
-                                    {buttonText}
-                            </button>
-                        </form>
-                    </div>
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const authType = this.props.signUp ? "signup" : "signin";
+    this.props
+      .onAuth(authType, this.state)
+      .then(() => {
+        this.props.history.push("/");
+      })
+      .catch(() => {
+        return;
+      });
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  render() {
+    const { email, username, password, profileImageUrl } = this.state;
+    const {
+      signUp,
+      heading,
+      buttonText,
+      errors,
+      history,
+      removeError
+    } = this.props;
+
+    history.listen(() => {
+      removeError();
+    });
+
+    return (
+      <div>
+        <div className="row justify-content-md-center text-center">
+          <div className="col-md-6">
+            <form onSubmit={this.handleSubmit}>
+              <h2>{heading}</h2>
+              {errors.message && (
+                <div className="alert alert-danger">{errors.message}</div>
+              )}
+              <label htmlFor="email">E-mail</label>
+              <input
+                autoComplete="off"
+                className="form-control"
+                id="email"
+                name="email"
+                onChange={this.handleChange}
+                type="text"
+                value={email}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                autoComplete="off"
+                className="form-control"
+                id="password"
+                name="password"
+                onChange={this.handleChange}
+                type="password"
+                value={password}
+              />
+              {signUp && (
+                <div>
+                  <label htmlFor="username">Username</label>
+                  <input
+                    autoComplete="off"
+                    className="form-control"
+                    id="username"
+                    name="username"
+                    onChange={this.handleChange}
+                    type="text"
+                    value={username}
+                  />
+                  <label htmlFor="image-url">Image URL</label>
+                  <input
+                    autoComplete="off"
+                    className="form-control"
+                    id="image-url"
+                    name="profileImageUrl"
+                    onChange={this.handleChange}
+                    type="text"
+                    value={profileImageUrl}
+                  />
                 </div>
-            </div>  
-        );
-    }
+              )}
+              <button
+                type="submit"
+                className="btn btn-primary btn-block btn-lg"
+              >
+                {buttonText}
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+Authform.propTypes = {
+  buttonText: PropTypes.string,
+  errors: PropTypes.object,
+  heading: PropTypes.string,
+  history: PropTypes.object,
+  onAuth: PropTypes.func,
+  signIn: PropTypes.bool,
+  removeError: PropTypes.func
+};
 
 export default Authform;
--- warbler-server/middleware/auth.js
+++ warbler-server/middleware/auth.js
@@ -5,6 +5,7 @@ exports.loginRequired = function(req, res, next) {
     try {
         const token = req.headers.authorization.split(" ")[1];
         jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
+            debugger
             if (decoded) {
                 return next();
             } else {
